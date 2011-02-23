a96ed0b1e5dc078b4787246349836ca28c26a5cf
Matthew Jording <mjording@opengotham.com>
2011-02-23 13:16:40 -0500

    first commit from template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackDraft::Application.config.secret_token = '4e663ffb391a7ac5a75d283643111eb4a9907f2dbd75ed9ca6fcb7af846492838fa12c14b60473d1da66a2d517f6c46bbf43609efbf6cf887c42631c41936dce'
